1dc2a7cb7aad14dd4c9e07e1aaed42b882016928
Nirmit Patel <55250776+Niks139@users.noreply.github.com>
2021-06-03 13:57:33 -0400

    Qualifications Done

--- scripts/main.js
+++ scripts/main.js
@@ -50,10 +50,26 @@ const skillsContent = document.getElementsByClassName('skills__content'),
       })
 
 /* QUALIFICATION TABS */
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
 
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
 
-/*==================== SERVICES MODAL ====================*/
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+    })
+})
+
+/* SERVICES MODAL */
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
